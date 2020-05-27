8079746f7a4b3b2fe6dcf34d27abf527321b4638
Tim <TimCPB@github.com>
2020-05-27 14:32:37 +0100

    installed shotgun gem

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ ruby '2.7.0'
 group :development, :test do
   gem "rspec"
   gem 'sinatra', '~> 2.0', '>= 2.0.8.1'
+  gem 'shotgun', '~> 0.9.2'
 end
--- app.rb
+++ app.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/' do
   "hello!"
 end
+
+get '/secret' do
+  "Oooh, a route called 'secret'!"
+end
+
+get '/new' do
+  "Yet another string"
+end
