0c9281e967ebd371584914f6b9f8d7cda3e1dcd8
Tim <TimCPB@github.com>
2020-05-27 15:55:40 +0100

    Used params to make everything interactive

--- lib/app.rb
+++ lib/app.rb
@@ -12,9 +12,18 @@ get '/new' do
   "Yet another string"
 end
 
-get '/cat' do
+get '/random-cat' do
 
 @name = ["Amigo", "Oscar", "Viking"].sample
 erb(:index)
 
+end
+
+get '/named-cat' do
+
+  p params[:name]
+  @name = params[:name]
+  erb(:index)
+
+  
 end
